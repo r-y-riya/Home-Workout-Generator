c60be0af02ce65bd524eb9626a46e8ace12a63b8
Cameron Kranz <cameronkranz2@gmail.com>
2024-09-19 15:42:28 -0400

    designed page theme

--- Workout_Generator.py
+++ Workout_Generator.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import pandas as pd
 import random
+import base64
+
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             border: 3px solid black;
+             border-top: 63px solid black;
+             background: url(data:image/{'jpg'};base64,{base64.b64encode(open('Data/background.jpg', "rb").read()).decode()});
+             background-size: cover
+
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
 def GenerateExercises(selected_exercises, number):
     count=0
@@ -12,12 +28,14 @@ def GenerateExercises(selected_exercises, number):
             count = count + 1
     return exercise_list 
 
+guide = 'Choose what muscles you want to hit and the number of exercises you want to do and a random workout will be generated for you. It is recommened to do 3 sets for each exercise but that, along with the recommended reps should be whatever you are comfortable with.'
+st.header('Random Workout Generator', divider='red', anchor=False, help=guide)
 exercises = pd.read_csv('Data/Exercises.csv') #load exercise file
-types = st.multiselect('Which muscle groups do you want to hit', options=['Chest', 'Shoulders', 'Arms', 'Back', 'Legs', 'Core'])
+types = st.multiselect('**Which muscle groups do you want to hit**', options=['Chest', 'Shoulders', 'Arms', 'Back', 'Legs', 'Core'])
 selected_exercises = exercises[exercises['type'].isin(types)] #filtered exercise list
-number = st.number_input("Number Of Exercises", step=1, min_value=0, max_value=len(selected_exercises))
+number = st.number_input("**Number Of Exercises**", step=1, min_value=0, max_value=len(selected_exercises))
 
-if st.button('Generate'):
+if st.button('**Generate**', type='primary'):
     exercise_list = GenerateExercises(selected_exercises, number)
     for index, row in exercise_list.iterrows():
         st.write(row)
