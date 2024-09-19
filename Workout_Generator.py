181ee12c113f79ad43ea2710c72046a0e411c970
cameron-kranz <cameronkranz2@gmail.com>
2024-09-18 21:47:44 -0400

    generate exercise and corosponding video

--- Workout_Generator.py
+++ Workout_Generator.py
@@ -4,10 +4,12 @@ import random
 
 def GenerateExercises(selected_exercises, number):
     count=0
+    exercise_list = []
     while count < number:
         rand = random.randrange(0,len(selected_exercises)) #choose a random number from range of selected exercise list length
-        st.write(selected_exercises.iloc[rand])
+        exercise_list.append(selected_exercises.iloc[rand])
         count = count + 1
+    return exercise_list
 
 exercises = pd.read_csv('Data/Exercises.csv') #load exercise file
 types = st.multiselect('Which muscle groups do you want to hit', options=['Chest', 'Shoulders', 'Arms', 'Back', 'Legs', 'Core'])
@@ -15,7 +17,7 @@ selected_exercises = exercises[exercises['type'].isin(types)] #filtered exercise
 number = st.number_input("Number Of Exercises", step=1, min_value=0, max_value=len(selected_exercises))
 
 if st.button('Generate'):
-    GenerateExercises(selected_exercises, number)
-
-
-
+    exercise_list = GenerateExercises(selected_exercises, number)
+    for x in exercise_list:
+        st.write(x)
+        st.video('Data/ExerciseVids/' + x['name'].lower().replace(" ", "") + '.MOV', loop=True, autoplay=True, muted=True)
