03351556d6c72c9fd7dc61281f430dff14ed70b7
Cameron Kranz <cameronkranz2@gmail.com>
2024-09-20 13:52:30 -0400

    if exercise count allows it, require one exercise from each muscle group selected to be generated

--- Workout_Generator.py
+++ Workout_Generator.py
@@ -18,27 +18,28 @@ st.markdown(
          unsafe_allow_html=True
      )
 
-def GenerateExercises(selected_exercises, number):
+def GenerateExercises(selected_muscles, number):
     count=0
     exercise_list = pd.DataFrame(columns=['name', 'type', 'description', 'reps'])
     while count < number:
-        rand = random.randrange(0,len(selected_exercises)) #choose a random number from range of selected exercise list length
-        if selected_exercises.iloc[rand]['name'] not in exercise_list['name'].tolist():
-            exercise_list.loc[len(exercise_list)] = selected_exercises.iloc[rand]
-            count = count + 1
+        rand = random.randrange(0,len(selected_muscles)) #choose a random number from range of selected exercise list length
+        if selected_muscles.iloc[rand]['name'] not in exercise_list['name'].tolist():
+            if (set(exercise_list['type']) == set(selected_muscles['type'])) or (selected_muscles.iloc[rand]['type'] not in set(exercise_list['type'])):
+                exercise_list.loc[len(exercise_list)] = selected_muscles.iloc[rand]
+                count = count + 1
     return exercise_list 
 
 guide = 'Choose what muscles you want to hit and the number of exercises you want to do and a random workout will be generated for you. It is recommened to do 3 sets for each exercise but that, along with the recommended reps should be whatever you are comfortable with.'
 st.header('At-Home Workout Generator', divider='red', anchor=False, help=guide)
 exercises = pd.read_csv('Data/Exercises.csv') #load exercise file
 types = st.multiselect('**Which muscle groups do you want to hit**', options=['Chest', 'Shoulders', 'Arms', 'Back', 'Legs', 'Core'])
-selected_exercises = exercises[exercises['type'].isin(types)] #filtered exercise list
-number = st.number_input("**Number Of Exercises**", step=1, min_value=0, max_value=len(selected_exercises))
+selected_muscles = exercises[exercises['type'].isin(types)] #filtered exercise list
+number = st.number_input("**Number Of Exercises**", step=1, min_value=0, max_value=len(selected_muscles))
 
 if st.button('**Generate**', type='primary'):
-    exercise_list = GenerateExercises(selected_exercises, number)
+    exercise_list = GenerateExercises(selected_muscles, number)
     for index, row in exercise_list.iterrows():
-        st.subheader(f'**{row['name']} : 3 sets x {row['reps']}**')
+        st.subheader(f'**{row['name']} : 3 sets x {row['reps']}**', anchor=False)
         with st.expander('**DEMO**'):
             st.markdown(row['description'])
             st.video('Data/ExerciseVids/' + row['name'].lower().replace(" ", "") + '.MOV', loop=True, autoplay=True, muted=True)
