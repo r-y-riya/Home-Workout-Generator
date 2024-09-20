e3241cdbb3640de5bea80257d3fbc85516b87e3a
Cameron Kranz <cameronkranz2@gmail.com>
2024-09-20 14:33:18 -0400

    change font size

--- Workout_Generator.py
+++ Workout_Generator.py
@@ -11,12 +11,19 @@ st.markdown(
              border-top: 63px solid black;
              background: url(data:image/{'jpg'};base64,{base64.b64encode(open('Data/background.jpg', "rb").read()).decode()});
              background-size: cover
-
          }}
          </style>
          """,
          unsafe_allow_html=True
      ) #set background image and page border
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:20px !important;
+}
+</style>
+""", unsafe_allow_html=True)
 
 def GenerateExercises(selected_muscles, number):
     count=0
@@ -39,7 +46,7 @@ number = st.number_input("**Number Of Exercises**", step=1, min_value=0, max_val
 if st.button('**Generate**', type='primary'): #generate exercises button
     exercise_list = GenerateExercises(selected_muscles, number) #generate random exercises
     for index, row in exercise_list.iterrows(): #for each exercise chosen
-        st.subheader(f'**{row['name']} : 3 sets x {row['reps']}**', anchor=False) #display exercise and setsxreps
+        st.markdown(f'<p class="big-font"><b>{row['name']}: 3 sets x {row['reps']}<b></p>',  unsafe_allow_html=True) #display exercise and setsxreps
         with st.expander('**DEMO**'): #more demo and description expander
             st.markdown(row['description']) #show exercise description
-            st.video('Data/ExerciseVids/' + row['name'].lower().replace(" ", "") + '.MOV', loop=True, autoplay=True, muted=True) #play demo video
+            st.video('Data/ExerciseVids/' + row['name'].lower().replace(" ", "") + '.MOV', loop=True, autoplay=False, muted=True) #play demo video
